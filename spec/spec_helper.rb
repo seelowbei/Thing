d281d92be75eb66a06bd448dbcec1a6dfbf85894
SB <seelowbei@gmail.com>
2014-03-13 11:06:09 +0800

    added code climate test coverage check

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,9 @@ require 'rspec/autorun'
 # rails generate rspec:install ==> to initialize rspec dir and spec_helper.rb
 # rake db:test:prepare ==>initialize rspec test db
 
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
